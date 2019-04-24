3dfaccf509bf1c9da419bf955d8c4b14df2eb7a7
Joyionline <hhaz1dyy888@vip.qq.com>
2019-04-24 10:38:48 +0800

    Add web handle

--- web/controllers/admin.go
+++ web/controllers/admin.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"log"
+	"stars/models"
 	"stars/services"
+	"time"
 
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/mvc"
@@ -14,21 +17,59 @@ type AdminController struct {
 
 // 获取数据
 func (c *AdminController) Get() mvc.Result {
-
-	return
+	datalist := c.Service.GetAll()
+	return mvc.View{
+		Name: "admin/index.html",
+		Data: iris.Map{
+			"Title":    "管理后台",
+			"Datalist": datalist,
+		},
+		Layout: "admin/layout.html",
+	}
 }
 
 func (c *AdminController) GetEdit() mvc.Result {
-
-	return nil
+	id, err := c.Ctx.URLParamInt("id")
+	var data *models.StarInfo
+	if err == nil {
+		data = c.Service.GetById(id)
+	}
+	return mvc.View{
+		Name: "admin/edit.html",
+		Data: iris.Map{
+			"Title": "管理后台",
+			"info":  data,
+		},
+	}
 }
 
 func (c *AdminController) PostSave() mvc.Result {
-
-	return nil
+	info := models.StarInfo{}
+	err := c.Ctx.ReadForm(&info)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if info.Id > 0 {
+		info.SysUpdated = int(time.Now().Unix())
+		c.Service.UpdateByUserInfo(
+			&info, []string{"name_zh", "name_en", "avatar", "birthday",
+				"height", "weight", "club", "jersy", "country", "moreinfo"},
+		)
+	} else {
+		info.SysCreated = int(time.Now().Unix())
+		c.Service.Create(&info)
+	}
+	return mvc.Response{
+		Path: "/admin/",
+	}
 }
 
 func (c *AdminController) GetDelete() mvc.Result {
-
-	return nil
+	id, err := c.Ctx.URLParamInt("id")
+	if err == nil {
+		c.Service.DeleteById(id)
+	}
+	return mvc.Response{
+		Path: "/admin",
+	}
 }
--- web/controllers/index.go
+++ web/controllers/index.go
@@ -1,1 +1,52 @@
 package controllers
+
+import (
+	"stars/services"
+
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/mvc"
+)
+
+type IndexController struct {
+	Ctx     iris.Context
+	Service services.StarsService
+}
+
+func (c *IndexController) Get() mvc.Result {
+	datalist := c.Service.GetAll()
+	return mvc.View{
+		Name: "index.html",
+		Data: iris.Map{
+			"Title":    "球星库",
+			"Datalist": datalist,
+		},
+	}
+}
+
+func (c *IndexController) GetById(id int) mvc.Result {
+	if id < 1 {
+		return mvc.Response{
+			Path: "/",
+		}
+	}
+	data := c.Service.GetById(id)
+	return mvc.View{
+		Name: "info.html",
+		Data: iris.Map{
+			"Title": "球星库",
+			"Data":  data,
+		},
+	}
+}
+
+func (c *IndexController) GetSearch() mvc.Result {
+	country := c.Ctx.URLParam("country")
+	datalist := c.Service.Search(country)
+	return mvc.View{
+		Name: "index.html",
+		Data: iris.Map{
+			"Title":    "球星库",
+			"Datalist": datalist,
+		},
+	}
+}
